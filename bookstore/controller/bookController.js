4afb585721acda885edb42c556de955c59ea31b0
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-08-06 13:32:01 +0530

    delete, update routes

---
+++ bookstore/controller/bookController.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import {Book} from '../models/book.js';
+
+export const createBook = async (req, res) => {
+    const { title, author, price, genre, inStock } = req.body;
+
+    try {
+        const book = await Book.create({
+            title,
+            author,
+            genre,
+            price,
+            inStock
+        });
+        res.status(201).json(book);
+    } catch (error) {
+        res.status(400).json({ message: 'Error creating book', error: error.message });
+    }
+};
+
+
+export const getBooks = async (req, res) => {
+    try {
+        const books = await Book.find({})
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching books', error: error.message });
+    }
+};
+
+
+export const updateBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true}
+    );
+    res.json(book);
+} catch(err){
+    res.status(500).json({error: error.message});
+}
+};
+
+export const deleteBook = async (req, res) => {
+    try {
+        
+        if(Book.find({_id: req.params.id})){
+        const books = await Book.deleteOne({_id: req.params.id})
+        res.status(200).json('Successfully deleted book')
+        }
+        else {
+            res.status(404).json('book do not exist')
+        }
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+}
--- bookstore/routes/books.js
+++ bookstore/routes/books.js
@@ -1,37 +1,16 @@
 import mongoose from 'mongoose';
-import express from 'express'
-import {Book} from "../models/book.js"
+import express from 'express';
+import { createBook, deleteBook, getBooks, updateBook } from '../controller/bookController.js';
 const router = express.Router();
 router.use(express.json());
 
-router.post('/books', async (req, res) => {
-    const { title, author, price, genre, inStock } = req.body;
+router.post('/books', createBook);
 
-    try {
-        const book = await Book.create({
-            title,
-            author,
-            genre,
-            price,
-            inStock
-        });
-        res.status(201).json(book);
-    } catch (error) {
-        res.status(400).json({ message: 'Error creating book', error: error.message });
-    }
-});
+router.get('/books', getBooks);
 
-router.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find({});
-        res.status(200).json(books);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching books', error: error.message });
-    }
-});
+router.put('/books/:id', updateBook);
 
-
-
+router.delete('/books/:id', deleteBook);
 
 
 
