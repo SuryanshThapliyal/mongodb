9337cf6473a603773360375af2e585706b9edea3
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-08-12 12:34:26 +0530

    full bookstore project done

--- bookstore/app.js
+++ bookstore/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import books from './routes/books.js';
+import helmet from 'helmet'
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
 app.use('/', books);
 
 
